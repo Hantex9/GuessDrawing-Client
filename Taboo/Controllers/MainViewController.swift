a26dd41e84dc995df026d5a4c1328f7adf5443ab
Alessandro Izzo <alessandro.izzo4@studenti.unina.it>
2018-06-21 21:18:35 +0200

    Auto stash before merge of "master" and "origin/master"
    Project initialization

---
+++ Taboo/Controllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Taboo
+//
+//  Created by Alessandro Izzo on 21/06/18.
+//  Copyright © 2018 Alessandro Izzo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }  
+}
